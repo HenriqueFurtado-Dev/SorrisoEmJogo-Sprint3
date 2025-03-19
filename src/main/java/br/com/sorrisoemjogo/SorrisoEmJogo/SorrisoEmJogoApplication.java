05282da3169cc8339cee09224ec768a0ecf60d4d
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2025-03-19 00:24:56 -0300

    Initial Commit

---
+++ src/main/java/br/com/sorrisoemjogo/SorrisoEmJogo/SorrisoEmJogoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sorrisoemjogo.SorrisoEmJogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorrisoEmJogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorrisoEmJogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sorrisoemjogo/SorrisoEmJogo/SorrisoEmJogoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sorrisoemjogo.SorrisoEmJogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorrisoEmJogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
