056449e88f16720244b7cf158f8aa04a51c0033a
Henrique Furtado <carloshenrique.furtado@hotmail.com>
2025-05-22 21:20:43 -0300

    Finalizado processamento da fila

--- src/main/java/br/com/sorrisoemjogo/SorrisoEmJogo/messaging/SorrisoConsumer.java
+++ src/main/java/br/com/sorrisoemjogo/SorrisoEmJogo/messaging/SorrisoConsumer.java
@@ -1,14 +1,23 @@
 package br.com.sorrisoemjogo.SorrisoEmJogo.messaging;
 
 import br.com.sorrisoemjogo.SorrisoEmJogo.config.RabbitConfig;
+import br.com.sorrisoemjogo.SorrisoEmJogo.model.MessageLog;
+import br.com.sorrisoemjogo.SorrisoEmJogo.repository.MessageLogRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SorrisoConsumer {
 
+    private final MessageLogRepository repo;
+
+    public SorrisoConsumer(MessageLogRepository repo) {
+        this.repo = repo;
+    }
+
     @RabbitListener(queues = RabbitConfig.QUEUE_NAME)
-    public void receive(String message) {
-        System.out.println("Mensagem recebida do RabbitMQ: " + message);
+    public void receive(String msg) {
+        System.out.println("RabbitMQ ► " + msg);
+        repo.save(new MessageLog(msg));
     }
 }
